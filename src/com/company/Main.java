ec295cc5c11e3b27914810b6e29b071ea9fc67cb
Cameron-Jade Mummery <cami.mummery@aexp.com>
2019-11-25 10:18:37 +0000

    part one of the generics task

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+import java.util.List;
+
+
+public class Main extends Counter{
+
+    public static void main(String[] args) {
+        Counter counter = new Counter();
+        System.out.println(counter.toString());
+    }
+
+
+}
